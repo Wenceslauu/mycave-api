a674d9ea2fe2637113396ad79ae342ac67805bde
Wenceslauu <joaovitor.campagninin@hotmail.com>
2022-11-11 14:43:48 -0300

    Fix Mongoose virtuals bug

--- models/Post.js
+++ models/Post.js
@@ -1,21 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const { DateTime } = require('luxon')
-
 const PostSchema = new Schema({
     text: { type: String, required: true},
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true},
-    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    date: { type: Date, required: true }
-})
-
-PostSchema.virtual('formatted-date').get(function() {
-    return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED)
-})
-
-PostSchema.virtual('url').get(function() {
-    return `/api/posts/${this._id}`
+    date: { type: Date, required: true },
+    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 })
 
 module.exports = mongoose.model('Post', PostSchema)
--- models/User.js
+++ models/User.js
@@ -7,16 +7,12 @@ const UserSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, minLength: 8, required: true },
     age: { type: Number, min: 12, required: true },
-    photo: { data: Buffer, contentType: String },
-    friends: [{ type: Schema.Types.ObjectId, ref: 'User'}],
+    photo: { type: String },
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User'}]
 })
 
 UserSchema.virtual('full_name').get(function() {
     return `${this.first_name} ${this.last_name}`
-})
-
-UserSchema.virtual('url').get(function() {
-    return `/api/users/${this._id}`
 })
 
 module.exports = mongoose.model('User', UserSchema)
