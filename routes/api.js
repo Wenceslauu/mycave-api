6d63d59f39db6595eb4479bcb6d8a62dcdc70f2e
Wenceslauu <joaovitor.campagninin@hotmail.com>
2022-11-11 14:46:11 -0300

    Lay out endpoints accordingly

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,34 @@ const router = express.Router()
 
 const userController = require('../controllers/userController')
 const friendRequestController = require('../controllers/friendRequestController')
+const postController = require('../controllers/postController')
+const commentController = require('../controllers/commentController')
 
 router.post('/signup', userController.signup)
 
 router.post('/login', userController.login)
 
-router.get('/users/:userID/', userController.profile)
+router.get('/posts', postController.posts)
+
+router.post('/posts', postController.writePost)
+
+router.delete('/posts/:postID', postController.deletePost)
+
+router.put('/posts/:postID/like', postController.toggleLike)
+
+router.get('/posts/:postID/comments', commentController.comments)
+
+router.post('/posts/:postID/comments', commentController.writeComment)
+
+router.delete('/posts/:postID/comments/:commentID', commentController.deleteComment)
+
+router.get('/users', userController.users)
+
+router.get('/users/:userID', userController.profile)
+
+router.get('/users/:userID/friends', userController.friends)
+
+router.post('/users/:userID/photo', userController.editPhoto)
 
 router.get('/users/:userID/friend-requests', friendRequestController.friendRequests)
 
