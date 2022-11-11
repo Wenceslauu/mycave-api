e4d881ce71eaf4dd9b3aa8712f22deef56a9a475
Wenceslauu <joaovitor.campagninin@hotmail.com>
2022-11-11 14:33:00 -0300

    Add posts, comments and likes

---
+++ controllers/commentController.js
@@ -0,0 +1,60 @@
+const { body, validationResult } = require("express-validator")
+const passport = require('passport')
+
+const Comment = require('../models/Comment')
+
+exports.comments = [
+    passport.authenticate('jwt', { session: false }),
+    (req, res, next) => {
+        Comment.find({ post: req.params.postID }).populate('user', ['username', 'photo']).sort({ date: 'desc' }).exec((err, comments) => {
+            if (err) return next(err)
+
+            res.status(200).json({ comments, number_of_comments: comments.length })
+        })
+    }
+]
+
+exports.writeComment = [
+    passport.authenticate('jwt', { session: false }),
+    body('text', 'Text required.').trim().notEmpty().escape(),
+    (req, res, next) => {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) 
+            res.status(400).json({ errors: errors.array() })
+        else {
+            const comment = new Comment({
+                text: req.body.text,
+                post: req.params.postID,
+                user: req.user._id,
+                date: Date.now()
+            })
+
+            comment.save((err) => {
+                if (err) return next(err)
+
+                res.status(200).end()
+            })
+        }
+    }
+]
+
+exports.deleteComment = [
+    passport.authenticate('jwt', { session: false }),
+    (req, res, next) => {
+        Comment.findById(req.params.commentID, 'user', (err, results) => {
+            if (err) return next(err)
+
+            if (!req.user._id.equals(results.user))
+                res.status(401).end()
+
+            else {
+                Comment.findByIdAndDelete(req.params.commentID, (err) => {
+                    if (err) return next(err)
+
+                    res.status(200).end()
+                })
+            }
+        })
+    }
+]
---
+++ controllers/postController.js
@@ -0,0 +1,91 @@
+const { body, validationResult } = require('express-validator')
+const passport = require('passport')
+const async = require('async')
+
+const Post = require('../models/Post')
+const Comment = require('../models/Comment')
+
+exports.posts = [
+    passport.authenticate('jwt', { session: false }),
+    (req, res, next) => {
+        Post.find({ $or: [{ user: { $in: req.user.friends }}, { user: req.user._id }]}).populate('user', ['username', 'photo']).sort({ date: 'desc' }).exec((err, posts) => {
+            if (err) return next(err)
+
+            res.status(200).json({ posts })
+        })
+    }
+]
+
+exports.writePost = [
+    passport.authenticate('jwt', { session: false }),
+    body('text', 'Text required.').trim().notEmpty().escape(),
+    (req, res, next) => {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty())
+            res.status(400).json({ errors: errors.array() })
+        else {
+            const post = new Post({
+                text: req.body.text,
+                user: req.user._id,
+                date: Date.now()
+            })
+
+            post.save((err) => {
+                if (err) return next(err)
+
+                res.status(200).end()
+            })
+        }
+    }
+]
+
+exports.deletePost = [
+    passport.authenticate('jwt', { session: false }),
+    (req, res, next) => {
+        Post.findById(req.params.postID, 'user', (err, result) => {
+            if (err) return next(err)
+
+            if (!req.user._id.equals(result.user))
+                res.status(401).end()
+            else {
+                async.parallel({
+                    del_post(cb) {
+                        Post.findByIdAndDelete(req.params.postID).exec(cb)
+                    },
+                    del_comments(cb) {
+                        Comment.deleteMany({ post: req.params.postID }).exec(cb)
+                    }
+                }, (err) => {
+                    if (err) return next(err)
+            
+                    res.status(200).end()
+                })
+            }
+        })
+    }
+]
+
+exports.toggleLike = [
+    passport.authenticate('jwt', { session: false }),
+    (req, res, next) => {
+        Post.findById(req.params.postID, (err, post) => {
+            if (err) return next(err)
+
+            const likeIndex = post.likes.findIndex((like) => {
+                return like.equals(req.user._id)
+            })
+
+            if (likeIndex === -1) 
+                post.likes.push(req.user._id)
+            else
+                post.likes.splice(likeIndex, 1) 
+            
+            post.save((err) => {
+                if(err) return next(err)
+
+                res.status(200).end()
+            })
+        })
+    }
+]
