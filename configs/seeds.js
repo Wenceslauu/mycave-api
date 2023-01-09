c688c1b9ec34846c947d11dcf627f3f7d1b4011c
Wenceslauu <joaovitor.campagninin@hotmail.com>
2023-01-09 17:52:54 -0300

    Create config to seed the DB

--- configs/seeds.js
+++ configs/seeds.js
@@ -1,27 +1,132 @@
 const { faker } = require('@faker-js/faker')
 
 const User = require('../models/User')
-const Post = requrie('../models/Post')
+const Post = require('../models/Post')
 const Comment = require('../models/Comment')
 
-const user = new User({
-    first_name: faker.name.firstName(),
-    last_name: faker.name.lastName(),
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    age: faker.datatype.number(50)
-})
+// database setup
+require('./mongo')
 
-// const post = new Post({
-//     text: faker.lorem.lines(),
-//     user: ,
-//     date: faker.date.soon(),
-//     likes: []
-// })
+const users = []
+const posts = []
+const comments = []
 
-// const comment = new Comment({
-//     text: faker.lorem.lines(),
-//     user: ,
-//     post: ,
-//     date: faker.date.soon()
-// })
+function createRandomUser() {
+    const genders = ['male', 'female']
+    const random = Math.floor(Math.random() * genders.length)
+    const randomGender = genders[random]
+
+    const first_name = faker.name.firstName(randomGender)
+    const last_name = faker.name.lastName(randomGender)
+
+    const user = new User({
+        first_name,
+        last_name,
+        username: faker.internet.userName(first_name, last_name),
+        password: faker.internet.password(),
+        age: faker.datatype.number({ min: 12, max: 50 }),
+        bio: faker.lorem.paragraph(),
+        isBot: true,
+        friends: []
+    })
+
+    users.push(user)
+}
+
+function createRandomPost(userID) {
+    const post = new Post({
+        text: faker.lorem.paragraph(),
+        user: userID,
+        date: faker.date.soon(),
+        likes: []
+    })
+
+    posts.push(post)
+}
+
+function createRandomComment(userID, postID) {
+    const comment = new Comment({
+        text: faker.lorem.paragraph(),
+        user: userID,
+        post: postID,
+        date: faker.date.soon()
+    })
+
+    comments.push(comment)
+}
+
+function addPosts() {
+    users.forEach((user) => {
+        for (let i = 0; i < Math.floor(Math.random() * 5); i++) {
+            createRandomPost(user._id)
+        }
+    })
+}
+
+function addComments() {
+    posts.forEach((post) => {
+        users.forEach((user) => {
+            if (Math.random() > 0.8)
+                createRandomComment(user._id, post._id)
+        })
+    })
+}
+
+function addLikes() {
+    posts.forEach((post) => {
+        users.forEach((user) => {
+            if (Math.random() > 0.6)
+                post.likes.push(user._id)
+        })
+    })
+}
+
+function addFriends() {
+    users.forEach((firstUser) => {
+        users.forEach((secondUser) => {
+            if ((!firstUser._id.equals(secondUser._id)) && (!firstUser.friends.includes(secondUser._id))) {
+                if (Math.random() > 0.75) {
+                    firstUser.friends.push(secondUser._id)
+                    secondUser.friends.push(firstUser._id)
+                }
+            }
+        })
+    })
+}
+
+function seed() {
+    for (let i = 0; i < 30; i++) {
+        createRandomUser()
+    }
+
+    addPosts()
+    addComments()
+    addLikes()
+    addFriends()
+
+    users.forEach(async (user) => {
+        try {
+            await user.save()
+        } catch (err) {
+            console.log(err)
+        }
+    })
+
+    posts.forEach(async (post) => {
+        try {
+            await post.save()
+        } catch (err) {
+            console.log(err)
+        }
+    })
+
+    comments.forEach(async (comment) => {
+        try {
+            await comment.save()
+        } catch (err) {
+            console.log(err)
+        }
+    })
+}
+
+seed()
