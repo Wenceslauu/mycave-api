4343adf41d0ef60a465751ff1fbfdde2f6fecb0e
Wenceslauu <joaovitor.campagninin@hotmail.com>
2023-01-10 16:20:57 -0300

    Disable automatic index creation

--- configs/mongo.js
+++ configs/mongo.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 const mongoDB = process.env.DB_URL_STRING
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: false })
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
